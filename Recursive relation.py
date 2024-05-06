c9076152823ab6cf615182f03ad4788b614112e1
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-05-06 04:45:17 -0400

    Add files via upload

---
+++ Recursive relation.py
@@ -0,0 +1,25 @@
+def Fibonacci(n, k):
+
+    if n < 0:
+
+        print("Incorrect input")
+
+    elif n == 0:
+
+        return 0
+
+    elif n == 1:
+
+        return 1
+        
+    elif n==2: 
+        return k
+
+    elif n<=4:
+        return Fibonacci(n-1,k) + Fibonacci(n-2,k)
+    
+    else: 
+        return Fibonacci(n-1,k) + Fibonacci(n-2,k)*k
+
+
+print(Fibonacci(5, 3))
