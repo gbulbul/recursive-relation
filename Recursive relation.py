c695ef3bc7ef87344b3da4c07efe96b36231f28d
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-12 18:34:41 -0400

    Update Recursive relation.py

--- Recursive relation.py
+++ Recursive relation.py
@@ -1,29 +1,20 @@
 #n = number of generation passed, k= number of offsprings which are allowed/determined in each litter.
-def Fibonacci(n, k):
-
-    if n < 0:
-
-        print("Incorrect input")
-
-    elif n == 0:
-
-        return 0
-
-    elif n == 1:
-
-        return 1
-        
-    elif n == 2: 
-        
-        return k
-
-    elif n <=4:
-        
-        return Fibonacci(n-1,k) + Fibonacci(n-2,k)
-    
-    else: 
-        
-        return Fibonacci(n-1,k) + Fibonacci(n-2,k)*k
+class recursionClass:
+    def Fibonacci(n, k):
+        if n < 0:
+           print("Incorrect input")
+        elif n == 0:
+           return 0
+        elif n == 1:
+           return 1
+        elif n == 2: 
+           return k
+        elif n <=4:
+           return recursionClass.Fibonacci(n-1,k) + recursionClass.Fibonacci(n-2,k)
+        else: 
+           return recursionClass.Fibonacci(n-1,k) + recursionClass.Fibonacci(n-2,k)*k
 
 
-print(Fibonacci(5, 3))
+if __name__=="__main__":
+    n,k=5,3
+    print(recursionClass.Fibonacci(n,k))
